44e5839370c225fcc9ec510bef1ab141588a3321
HDR <refshet@gmail.com>
2019-08-10 05:10:11 +0200

    - Added a command called "addreactions" that adds all listed emoji id's from the config to a specified message (Requires the minimum admin rank set in the config)

--- cogs/reactionranker.py
+++ cogs/reactionranker.py
@@ -5,8 +5,35 @@ from discord import *
 
 class reactionranker(commands.Cog):
 
+    least_role_needed = get_section("bot").get("admin_minimum_role")
+
     def __init__(self, bot):
         self.bot = bot
+
+    def has_at_least_role(name):
+        def predicate(ctx):
+            msg = ctx.message
+            ch = msg.channel
+            if type(ch) == DMChannel:
+                return False
+
+            role = utils.get(ctx.guild.roles, name=name)
+
+            return any([x >= role for x in msg.author.roles])
+
+        return commands.check(predicate)
+
+    @commands.command(aliases=["addreactions"])
+    @has_at_least_role(least_role_needed)
+    async def addreact(self, ctx, messageid: int):
+        """Adds base reactions to specified message (Admin only)"""
+
+        if messageid is not None:
+            msg = await ctx.fetch_message(id=messageid)
+            for emojiId in get_section("bot").get("ranks"):
+                print(emojiId)
+                emojis = '<:name:' + str(emojiId) + '>'
+                await msg.add_reaction(emojis)
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
